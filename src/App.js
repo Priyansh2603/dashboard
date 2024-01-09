282c8dfa37322ec0bb283a46481c08cebca2ece3
Priyansh2603 <ishudaksh2603@gmail.com>
2024-01-09 21:27:17 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import { useState } from 'react'
-
+import Payment from './Payment'
 
 const smVariant = { navigation: 'drawer', navigationButton: true }
 const mdVariant = { navigation: 'sidebar', navigationButton: false }
 
-export default function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false)
-  const variants = useBreakpointValue({ base: smVariant, md: mdVariant })
-
-  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen)
-
-  return (
+export default function App() {return (
     <>
       <Payment/>
     </>
